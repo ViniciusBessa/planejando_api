2c442b7072a84c3104bdd854e90cd0f9ee6cba48
Vinícius Bessa <vinicius.b.silva3@gmail.com>
2025-02-26 17:34:13 -0300

    fix: Access token adicionado para enviar o e-mail de recuperação

--- controllers/user.ts
+++ controllers/user.ts
@@ -243,6 +243,7 @@ const generatePasswordResetToken = asyncWrapper(
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN,
       },
     });
 
